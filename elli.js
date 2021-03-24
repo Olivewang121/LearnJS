6fa9ab36f337fb7b678b4ceefdf5d49e2068f4a6
Olive Wang <olivewang121@gmail.com>
2021-03-24 01:49:55 -0400

    move myMath to js file

--- elli.js
+++ elli.js
@@ -1,3 +1,25 @@
 function sayHello(){
     console.log('hello');
 }
+
+var myMath = {
+    PI: 3.141592657,
+    max: function () {
+        var max = arguments[0];
+        for (var i = 1; i < arguments.length; i++) {
+            if (max < arguments[i]) {
+                max = arguments[i];
+            }
+        }
+        return max;
+    },
+    min: function () {
+        var min = arguments[0];
+        for (var i = 1; i < arguments.length; i++) {
+            if (min > arguments[i]) {
+                min = arguments[i];
+            }
+        }
+        return min;
+    },
+}
